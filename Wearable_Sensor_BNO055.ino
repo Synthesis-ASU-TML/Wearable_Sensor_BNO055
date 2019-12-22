1f389df3890919ee6e9161afed4cc2abc0541c1d
Connor Rawls <connor.rawls@gmail.com>
2019-12-22 11:53:10 -0500

    Removed extra library

--- Wearable_Sensor_BNO055.ino
+++ Wearable_Sensor_BNO055.ino
@@ -22,7 +22,7 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
-#include <Adafruit_MQTT.h>
+#//include <Adafruit_MQTT.h>
 #include <utility/imumaths.h>
 #include "Connection_Variables.h" //Modify contents of file to change connection settings
 
